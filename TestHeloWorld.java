dcea5956ec2f5bd8c4d9d2b6bce513652e61fa16
MiñonJester <48932759+qwertyaccountforgithub@users.noreply.github.com>
2019-03-26 08:49:02 +0800

    Update TestHeloWorld.java

--- TestHeloWorld.java
+++ TestHeloWorld.java
@@ -6,5 +6,6 @@ public class TestHeloWorld {
             System.out.println("Ronnel Palenzuela");
             System.out.println("I've been here too.");
             System.out.println("Rohm Mendoza");
+            System.out.println("I've been here before a few times, and I'm quite aware we're dying");
    }
 }
